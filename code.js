4513b39ba71163a83288a6e1d4d85e28f254d60f
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-04-12 13:30:39 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,8 +1,29 @@
-function flip(array, n) {
+function flip(array, n)
+{
+    // If n > array.length, set n to array.length, so we flip the whole stack of pancakes.
+    if(n > array.length)
+    {
+        n = array.length;
+    }
+
+    //Move left towards right, n, and right, n, towards left, swapping sets of elements.
+    for(var left = 0; left < n; left++)
+    {
+        var temp = array[left];
+        array[left] = array[n - 1];
+        array[n - 1] = temp;
+        n--;
+    }
+    
     return array;
 }
 
 // Use only flip() here to manipulate the array
-function pancakeSort(array) {
+function pancakeSort(array)
+{
     return array;
 }
+
+var testArray = [1, 2, 3, 4];
+var result = flip(testArray, 3);
+console.log(result)
