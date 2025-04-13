9bb88ead3cc2a17cff77396ae9c47b89c155af45
Kodoka <10721764+Kodoka@users.noreply.github.com>
2025-04-13 14:01:55 -0600

    Update code.js

--- code.js
+++ code.js
@@ -1,6 +1,6 @@
 function flip(array, n)
 {
-    // If n > array.length, set n to array.length, so we flip the whole stack of pancakes.
+    // If n > array.length, set n to array.length, so we flip the whole stack of elements.
     if(n > array.length)
     {
         n = array.length;
@@ -18,12 +18,45 @@ function flip(array, n)
     return array;
 }
 
-// Use only flip() here to manipulate the array
+// Use only flip() here to manipulate the array.
 function pancakeSort(array)
 {
+    // For each element in the unsorted portion of the input array...
+    for(var unsortedPortion = array.length; unsortedPortion > 1; unsortedPortion--)
+    {
+        // Find the index of the max value.
+        var maxIndex = findMaxValue(array, unsortedPortion);
+        // If the max value isn't already at the end of the unsorted array portion...
+        if(maxIndex != unsortedPortion - 1)
+        {
+            // If the max value isn't at the start of the unsorted array portion...
+            if(maxIndex != 0)
+            {
+                // Move the largest element to the front of the array.
+                flip(array, maxIndex + 1);
+            }
+            // Move the largest element to the last position of the unsorted array portion.
+            flip(array, unsortedPortion);
+        }
+    }
+
     return array;
 }
 
-var testArray = [1, 2, 3, 4];
-var result = flip(testArray, 3);
-console.log(result)
+// Finds the max value in the unsorted portion of the array, and returns it's index.
+function findMaxValue(array, unsortedIndex)
+{
+    var maxIndex = 0;
+    // For each element in the unsorted portion o the array, check if array[i] is
+    // greater than the value at maxIndex...
+    for(var i = 0; i < unsortedIndex; i++)
+    {
+        // If it is, change the maxIndex value to the index of the greatest value.
+        if(array[i] > array[maxIndex])
+        {
+            maxIndex = i;
+        }
+    }
+
+    return maxIndex;
+}
